0ba523592e1730c478be3975efba99a289a4d43b
yanlibo <yaniibo2010@163.com>
2017-03-28 19:59:18 +0800

    Merge branch 'dev_ylb' of https://github.com/yanlibo2013/react_mobile
    # Conflicts:
    #	src/component/home/index.jsx

---
+++ src/component/henry/index.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+import {getDevicetype} from 'REDUX/action/common'
+import './index.less'//样式
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+    }
+    componentWillMount(){
+        let {dispatch}=this.props;
+        dispatch(getDevicetype());
+
+    }
+    render() {
+        return (
+            <div className="home">
+                <h1>henry 2017!</h1>
+            </div>
+        )
+    }
+}
+export default connect(state=>({
+
+}))(Home)
+
