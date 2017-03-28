e991350e79765f61765ec65ee60e553d4886848d
yanlibo <yaniibo2010@163.com>
2017-03-28 19:31:24 +0800

    modify home page

--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -16,7 +16,7 @@ class Home extends Component {
     render() {
         return (
             <div className="home">
-               <h1>home!</h1>
+               <h1>home2017!</h1>
             </div>
         )
     }
