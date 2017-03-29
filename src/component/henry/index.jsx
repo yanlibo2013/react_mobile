459ec3cc443b05b54bdc2633e504bc39fcf2674b
yanlibo <yanliboyanlibo@gomeholdings.com>
2017-03-29 14:10:40 +0800

    add module ylb2017

--- src/component/henry/index.jsx
+++ src/component/henry/index.jsx
@@ -16,7 +16,7 @@ class Home extends Component {
     render() {
         return (
             <div className="home">
-                <h1>henry 2017!</h1>
+                <h1>henry 2017!!!!!!</h1>
             </div>
         )
     }
