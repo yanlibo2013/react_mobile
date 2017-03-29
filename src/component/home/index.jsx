0a2ea34db2de18a24186089ed42e5ac08605398a
yanlibo <yaniibo2010@163.com>
2017-03-29 23:39:18 +0800

    Merge branch 'dev_henry' of https://github.com/yanlibo2013/react_mobile
    # Conflicts:
    #	src/component/home/index.jsx

--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -16,6 +16,7 @@ class Home extends Component {
     render() {
         return (
             <div className="home">
+                <h1>master</h1>
                <h1>henry 2019!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
             </div>
         )
