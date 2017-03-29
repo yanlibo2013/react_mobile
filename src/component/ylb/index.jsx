6e50c00c8babc04d766b9d5c17a365b61e995a55
yanlibo <yanliboyanlibo@gomeholdings.com>
2017-03-29 14:08:17 +0800

    add module ylb2017

---
+++ src/component/ylb/index.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { Tabs, WhiteSpace } from 'antd-mobile';
+
+const TabPane = Tabs.TabPane;
+
+function callback(key) {
+    console.log(key);
+}
+
+const TabExample = () => (
+    <div>
+        <div>ylb2017</div>
+    </div>
+);
+
+export default TabExample;
